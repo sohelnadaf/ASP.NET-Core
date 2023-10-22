ae2711ebf937acdfc637c31a5ebdb05e09f7db1e
Sohel Nadaf <sohelnadafb4u@gmail.com>
2023-10-22 11:50:29 +0530

    iniial file added

---
+++ CoreProjects/BasicLearning/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BasicLearning.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
